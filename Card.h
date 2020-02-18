387d633f22679e3992e2e971e31b0914f1a5e8f7
Kevin Monteith <kmonteith@student.bridgew.edu>
2020-02-17 22:43:46 -0500

    Created Main File

--- Card.h
+++ Card.h
@@ -1,5 +1,14 @@
 #pragma once
+#include <iostream>
 class Card
 {
+private:
+	std::string suit;
+	std::string rank;
+public:
+	std::string getSuit();
+	std::string getRank();
+	Card(std::string suit, std::string rank);
+	~Card();
 };
 
